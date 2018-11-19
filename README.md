d0fc0a0f41e3cb6821d5c92e53018b9dc5f58d17
Ozi <ozi.guerra96@gmail.com>
2018-11-19 09:27:35 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# expandable_card
+
+Flutter package for using expandable cards
+
+## Getting Started
+
+For help getting started with Flutter, view our online [documentation](https://flutter.io/).
+
+For help on editing package code, view the [documentation](https://flutter.io/developing-packages/).
---
+++ lib/expandable_card.dart
@@ -0,0 +1,22 @@
+library expandable_card;
+
+import 'package:flutter/material.dart';
+
+/// An expandable card.
+class ExpandableCard extends StatefulWidget{
+
+  @override
+  _ExpandableClassState createState() => _ExpandableClassState();
+  //TODO: create an expandable card
+
+}
+
+class _ExpandableClassState extends State<ExpandableCard>
+{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+
+}
