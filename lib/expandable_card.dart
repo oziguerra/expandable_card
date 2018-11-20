fda9d2e104fbd5922cd14d6d0ffa6f733d28d7d5
Ozi <ozi.guerra96@gmail.com>
2018-11-20 09:49:38 -0800

    Added button that if pressed, changes the size of the now used animated container

--- lib/expandable_card.dart
+++ lib/expandable_card.dart
@@ -4,19 +4,53 @@ import 'package:flutter/material.dart';
 
 /// An expandable card.
 class ExpandableCard extends StatefulWidget{
-
   @override
   _ExpandableClassState createState() => _ExpandableClassState();
-  //TODO: create an expandable card
-
 }
 
 class _ExpandableClassState extends State<ExpandableCard>
 {
+  double boxHeight = 200.0;
+  bool isSmall = true;
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return null;
+    return
+      AnimatedContainer(
+        duration: Duration(seconds:1),
+        height: boxHeight,
+        width: 200.0,
+        //constraints: BoxConstraints.expand(),
+        child: Card(
+            elevation: 20.0,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Center(
+                  child: RaisedButton(
+                    onPressed:() {
+                      changeSize();
+                    },
+                    child: Text("Press Me!"),
+                  ),
+
+                )
+              ],
+            )
+        ),
+      );
   }
 
+  void changeSize () {
+    setState(() {
+      if (isSmall) {
+        boxHeight = 400.0;
+        isSmall = !isSmall;
+      }
+      else {
+        boxHeight = 200.0;
+        isSmall = !isSmall;
+      }
+    });
+  }
 }
--- test/expandable_card_test.dart
+++ test/expandable_card_test.dart
@@ -1,13 +1,54 @@
-import 'package:flutter_test/flutter_test.dart';
-
+import 'package:flutter/material.dart';
 import 'package:expandable_card/expandable_card.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final expandableCard = ExpandableCard();
-    /*expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);*/
-  });
+
+  runApp(expandablecard());
+
+}
+
+class expandablecard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: 'Meme Board',
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: Colors.blue[700],
+        accentColor: Colors.blue[50],
+      ),
+      home: HomePage(),
+    );
+  }
+
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Camp organizer'),
+      ),
+      body: SafeArea(
+          child: Center(
+              child: new ExpandableCard(),
+
+          )
+      ),
+    );
+  }
+
+
+
+
+
 }
